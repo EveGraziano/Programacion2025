07301258901724a22412cb96ca42b0a9a4980168
Evelyn <evelynbelengrazianomolinas@gmail.com>
2025-06-26 15:59:37 -0300

    TP°17

---
+++ TP°17/Script/script1.js
@@ -0,0 +1,13 @@
+console.log("Soy el archivo externo, hola");
+console.info("Soy un info");
+console.warn("Soy un warn");
+console.error("Soy un error");
+
+console.table({
+    Nombre: "Evelyn",
+    Apellido: "Graziano",
+    Curso: "Programacion",
+}
+);
+
+window.prompt("Ingrese un dato");
---
+++ TP°17/Script/script2.js
@@ -0,0 +1,46 @@
+let miVariable = "Info";
+
+console.log(miVariable);
+
+miVariable = 558;
+
+console.log(miVariable);
+
+const apellido = "Graziano"
+console.log(apellido);
+
+/* apellido = "Molinas";
+console.log(apellido); */
+
+const nombre = "Evelyn";
+
+console.log(nombre, apellido);
+
+const nombeCompleto = nombre + apellido;
+console.log(nombeCompleto);
+
+/* const dni = window.prompt("Ingrese su DNI");
+console.log(dni);*/
+
+let numeroA = 6;
+let numeroB = 4;
+let suma = numeroA + numeroB;
+console.log(suma);
+
+/* let numeroC = window.prompt("Primer numero");
+numeroC = Number(numeroC); //Parsear
+let numeroD = window.prompt("Segundo numero");
+numeroD = Number(numeroD);
+let sumaCD = numeroC + numeroD;
+console.log(sumaCD); */
+
+// Scoop
+
+let numeroX = 17;
+console.log(numeroX);
+{
+    let numeroX = 18;
+    console.log(numeroX);
+}
+
+console.log(numeroX);
